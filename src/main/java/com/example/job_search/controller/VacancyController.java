15722a424f712d97765fb442cd43bf72c90712bf
Maria <mary.chemikos@gmail.com>
2026-06-01 16:08:49 +0300

    add pagination

--- src/main/java/com/example/job_search/controller/VacancyController.java
+++ src/main/java/com/example/job_search/controller/VacancyController.java
@@ -1,22 +1,32 @@
 package com.example.job_search.controller;
 
 import com.example.job_search.parser.WorkUaParserService;
-import com.example.job_search.service.VacancyService;
+import com.example.job_search.entity.Vacancy;
+import com.example.job_search.repository.VacancyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class VacancyController {
-    private final VacancyService vacancyService;
+    private final VacancyRepository vacancyRepository;
     private final WorkUaParserService parserService;
 
     @GetMapping("/")
-    public String showHomePage(Model model) {
-        model.addAttribute("vacancies", vacancyService.getAllVacancies());
+    public String index(@RequestParam(defaultValue = "0") int page, Model model) {
+        int pageSize = 12;
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by("id").descending());
+        Page<Vacancy> vacancyPage = vacancyRepository.findAll(pageable);
+        model.addAttribute("vacancies", vacancyPage.getContent());
+        model.addAttribute("vacancyPage", vacancyPage);
         return "index";
     }
 
