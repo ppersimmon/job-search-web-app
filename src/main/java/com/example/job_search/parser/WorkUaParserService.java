a204586b2ae02eadd93abbed6d282c83ffcc3325
Maria <mary.chemikos@gmail.com>
2026-06-04 11:48:12 +0300

    fix HTTP headers

--- src/main/java/com/example/job_search/parser/WorkUaParserService.java
+++ src/main/java/com/example/job_search/parser/WorkUaParserService.java
@@ -51,8 +51,19 @@ public class WorkUaParserService {
 
                 try {
                     Document doc = Jsoup.connect(urlWithPage)
-                            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
-                            .timeout(5000)
+                            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                            .referrer("https://www.google.com/")
+                            .header("Accept-Language", "uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7")
+                            .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+                            .header("Sec-Ch-Ua", "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"")
+                            .header("Sec-Ch-Ua-Mobile", "?0")
+                            .header("Sec-Ch-Ua-Platform", "\"Windows\"")
+                            .header("Sec-Fetch-Dest", "document")
+                            .header("Sec-Fetch-Mode", "navigate")
+                            .header("Sec-Fetch-Site", "cross-site")
+                            .header("Sec-Fetch-User", "?1")
+                            .header("Upgrade-Insecure-Requests", "1")
+                            .timeout(10000)
                             .get();
 
                     Elements vacancyCards = doc.select(".card-hover");
