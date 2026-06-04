2cc66e411fafe144f32491d64cd7980fe4476cf0
Maria <mary.chemikos@gmail.com>
2026-06-04 11:06:15 +0300

    update schedule

--- src/main/java/com/example/job_search/parser/WorkUaParserService.java
+++ src/main/java/com/example/job_search/parser/WorkUaParserService.java
@@ -27,7 +27,7 @@ public class WorkUaParserService {
     private final CompanyRepository companyRepository;
     private final CategoryRepository categoryRepository;
 
-    @Scheduled(cron = "0 30 7 * * *")
+    @Scheduled(cron = "0 30 8 * * *")
     public void parseDiverseVacancies() {
         Map<String, String> categoriesMap = getCategoriesMap();
 
